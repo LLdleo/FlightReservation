af808fb59cc2f5edafdfdcfff5a7fa34fe5ebb88
Lidian Lin <lldlld0574@gmail.com>
2020-03-26 13:19:54 -0400

    now can return json array normally

--- src/test/HTTPServer.java
+++ src/test/HTTPServer.java
@@ -33,80 +33,58 @@ public class HTTPServer {
                 String[] requests = request.split("\\?");
                 HashMap<String, String> parameterPairs = new HashMap<>();
 
-                OutputStream os = socket.getOutputStream();
-                if (requests.length>1) {
-                    String[] parameters = requests[1].split("&");
-                    for (String parameter: parameters) {
-                        String[] pair = parameter.split("=");
-                        parameterPairs.put(pair[0], pair[1]);
-                    }
-                    System.out.println(parameterPairs);
-                    if (parameterPairs.get("action").equals("list")){
-                        if (parameterPairs.get("listType").equals("airports")) {
-                            Airports airports = ServerInterface.INSTANCE.getAirports(parameterPairs.get("teamName"));
-                            Collections.sort(airports);
-                            for (Airport airport : airports) {
-                                os.write((airport.toString()+"\r\n").getBytes());
-                                System.out.println(airport.toString());
-                            }
+
+                try{
+                    if (requests.length>1) {
+                        String[] parameters = requests[1].split("&");
+                        for (String parameter: parameters) {
+                            String[] pair = parameter.split("=");
+                            parameterPairs.put(pair[0], pair[1]);
                         }
-                        else if (parameterPairs.get("listType").equals("departing") || parameterPairs.get("listType").equals("arriving")) {
-                            Flights flights = ServerInterface.INSTANCE.getFlights(parameterPairs.get("teamName"), parameterPairs.get("listType"), parameterPairs.get("airport"), parameterPairs.get("day"));
-                            Collections.sort(flights);
-                            os.write("[".getBytes());
-                            for (Flight flight: flights) {
-                                os.write((flight.toJson()+",").getBytes());
-                                System.out.println(flight.toJson());
+                        System.out.println(parameterPairs);
+                        if (parameterPairs.get("action").equals("list")){
+                            if (parameterPairs.get("listType").equals("airports")) {
+                                Airports airports = ServerInterface.INSTANCE.getAirports(parameterPairs.get("teamName"));
+                                Collections.sort(airports);
+                                StringBuilder ssss = null;
+                                for (Airport airport : airports) {
+                                    ssss.append(airport.toString()).append("\r\n");
+                                    System.out.println(airport.toString());
+                                }
                             }
-                            os.write("]".getBytes());
-                        }
-                    }
-                }
-//                try{
-//                    if (requests.length>1) {
-//                        String[] parameters = requests[1].split("&");
-//                        for (String parameter: parameters) {
-//                            String[] pair = parameter.split("=");
-//                            parameterPairs.put(pair[0], pair[1]);
-//                        }
-//                        System.out.println(parameterPairs);
-//                        if (parameterPairs.get("action").equals("list")){
-//                            if (parameterPairs.get("listType").equals("airports")) {
-//                                Airports airports = ServerInterface.INSTANCE.getAirports(parameterPairs.get("teamName"));
-//                                Collections.sort(airports);
-//                                for (Airport airport : airports) {
-//                                    os.write((airport.toString()+"\r\n").getBytes());
-//                                    System.out.println(airport.toString());
-//                                }
-//                            }
-//                            else if (parameterPairs.get("listType").equals("departing") || parameterPairs.get("listType").equals("arriving")) {
-//                                Flights flights = ServerInterface.INSTANCE.getFlights(parameterPairs.get("teamName"), parameterPairs.get("listType"), parameterPairs.get("airport"), parameterPairs.get("day"));
-//                                Collections.sort(flights);
+                            else if (parameterPairs.get("listType").equals("departing") || parameterPairs.get("listType").equals("arriving")) {
+                                Flights flights = ServerInterface.INSTANCE.getFlights(parameterPairs.get("teamName"), parameterPairs.get("listType"), parameterPairs.get("airport"), parameterPairs.get("day"));
+                                Collections.sort(flights);
+                                StringBuilder ssss = new StringBuilder("[");
 //                                os.write("[".getBytes());
-//                                for (Flight flight: flights) {
+                                for (Flight flight: flights) {
+                                    ssss.append(flight.toJson()).append(",");
 //                                    os.write((flight.toJson()+",").getBytes());
-//                                    System.out.println(flight.toString());
-//                                }
-//                                os.write("]".getBytes());
-//                            }
-//                        }
-//                    }
-//
-////                    os.write("HTTP/1.1 200 OK\r\n".getBytes());
-////                    os.write("Content-Type:text/html;charset=utf-8\r\n".getBytes());
-////                    os.write("Content-Length:38\r\n".getBytes());
-////                    os.write("Server:gybs\r\n".getBytes());
-////                    os.write(("Date:"+new Date()+"\r\n").getBytes());
-////                    os.write("\r\n".getBytes());
-////                    os.write("<body>".getBytes());
-////                    os.write("<h1>hello! </h1>".getBytes());
-////                    os.write("<h3>HTTP Server! </h3>".getBytes("utf-8"));
-////                    os.write("</body>".getBytes());
-//                }
-//                catch (Exception e) {
-//                    System.out.println(e);;
-//                }
-                os.close();
+                                    System.out.println(flight.toJson());
+                                }
+                                ssss.append("]");
+                                System.out.println(ssss);
+
+                                OutputStream os = socket.getOutputStream();
+                                os.write("HTTP/1.1 200 OK\r\n".getBytes());
+                                os.write("Content-Type:json;charset=utf-8\r\n".getBytes());
+                                os.write("Server:gybs\r\n".getBytes());
+                                os.write(("Date:"+new Date()+"\r\n").getBytes());
+                                os.write("\r\n".getBytes());
+                                os.write("<body>".getBytes());
+                                os.write("<a>".getBytes());
+                                os.write(ssss.toString().getBytes());
+                                os.write("</a>".getBytes());
+                                os.write("</body>".getBytes());
+                                os.close();
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception e) {
+                    System.out.println(e);;
+                }
                 socket.close();
             }
         } catch (IOException e) {
