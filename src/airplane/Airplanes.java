ed3ec02d6179a68fe0a1eb631a353a4c9b63276e
Lidian Lin <lldlld0574@gmail.com>
2020-04-21 15:08:02 -0400

    add airplane packages and classes

---
+++ src/airplane/Airplanes.java
@@ -0,0 +1,18 @@
+package airplane;
+
+import java.util.ArrayList;
+
+/**
+ * This class aggregates a number of Airplanes. The aggregate is implemented as an ArrayList.
+ * Airplanes can be added to the aggregate using the ArrayList interface. Objects can
+ * be removed from the collection using the ArrayList interface.
+ *
+ * @author PoLYmer
+ * @version 1.0
+ * @since 2020-04-19
+ *
+ */
+
+public class Airplanes extends ArrayList<Airplane> {
+    private static final long serialVersionUID = 1L;
+}
